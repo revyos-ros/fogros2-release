f0b2f5ecc5d8404bcd6ea09b9b333210be922fe6
Karthik Dharmarajan <kdharmarajan@berkeley.edu>
2022-05-15 23:53:31 -0700

    Revert "Trimmed the branch to only the fogros2_examples sub directory"
    This reverts commit 619d5bac8c8414aa6e28997969e6a1f2d4f056d4.

---
+++ fogros2/fogros2/verb/ssh.py
@@ -0,0 +1,66 @@
+import os
+
+import boto3
+from botocore.exceptions import NoRegionError
+from ros2cli.verb import VerbExtension
+
+from ..util import instance_dir
+
+
+class SSHVerb(VerbExtension):
+    def add_arguments(self, parser, cli_name):
+        parser.add_argument("name", type=str, nargs=1, help="FogROS instance name to connect to")
+        parser.add_argument("--region", nargs="*", help="Set AWS region (overrides config/env settings)")
+        parser.add_argument(
+            "--user", "-u", type=str, nargs="?", default="ubuntu", help="User name of the remote SSH instance"
+        )
+
+    def query_region(self, region, name):
+        try:
+            client = boto3.client("ec2", region)
+        except NoRegionError:
+            raise RuntimeError("AWS is not configured! Please run `aws configure` first.")
+
+        ec2_instances = client.describe_instances(
+            Filters=[
+                {"Name": "instance.group-name", "Values": ["FOGROS2_SECURITY_GROUP"]},
+                {"Name": "tag:FogROS2-Name", "Values": name},
+            ]
+        )
+        return ec2_instances
+
+    def main(self, *, args):
+        regions = args.region
+
+        if regions is None or len(regions) == 0:
+            regions = [None]
+        elif "*" in regions or "all" in regions:
+            client = boto3.client("ec2")
+            response = client.describe_regions()
+            regions = [r["RegionName"] for r in response["Regions"]]
+
+        if len(regions) == 1:
+            instances = [self.query_region(regions[0], args.name)]
+        else:
+            from concurrent.futures import ThreadPoolExecutor, as_completed
+
+            with ThreadPoolExecutor(max_workers=len(regions)) as executor:
+                futures = [executor.submit(self.query_region, r, args.name) for r in regions]
+                instances = [f.result() for f in as_completed(futures)]
+
+        for ec2_instances in instances:
+            for res in ec2_instances["Reservations"]:
+                for inst in res["Instances"]:
+                    tag_map = {t["Key"]: t["Value"] for t in inst["Tags"]} if "Tags" in inst else {}
+                    name = tag_map["FogROS2-Name"]
+                    key_name = inst["KeyName"]
+                    key_path = os.path.join(instance_dir(), name, key_name + ".pem")
+                    if "PublicIpAddress" not in inst:
+                        print(f"Warning: matching instance does not have a public IP address")
+                        continue
+                    public_ip = inst["PublicIpAddress"]
+                    os.execvp("ssh", ("ssh", "-i", key_path, f"{args.user}@{public_ip}"))
+
+        # Since execvp replaces the current process, if here, we
+        # haven't found a matching instance.
+        print("No matching instance found")
