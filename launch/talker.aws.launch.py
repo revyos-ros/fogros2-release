0d72b5b8001f79524ac68f0985d8cf99e559dd87
Karthik Dharmarajan <kdharmarajan@berkeley.edu>
2022-05-15 23:54:16 -0700

    Rebase from 'debian/humble/fogros2_examples'

--- launch/talker.aws.launch.py
+++ launch/talker.aws.launch.py
@@ -1,3 +1,5 @@
+# Copyright 2022 The Regents of the University of California (Regents)
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -9,10 +11,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Copyright ©2022. The Regents of the University of California (Regents).
+# All Rights Reserved. Permission to use, copy, modify, and distribute this
+# software and its documentation for educational, research, and not-for-profit
+# purposes, without fee and without a signed licensing agreement, is hereby
+# granted, provided that the above copyright notice, this paragraph and the
+# following two paragraphs appear in all copies, modifications, and
+# distributions. Contact The Office of Technology Licensing, UC Berkeley, 2150
+# Shattuck Avenue, Suite 510, Berkeley, CA 94720-1620, (510) 643-7201,
+# otl@berkeley.edu, http://ipira.berkeley.edu/industry-info for commercial
+# licensing opportunities. IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+# INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+# DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE. REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+# PROVIDED HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+# MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 from launch_ros.actions import Node
 
-from fogros2 import AWS, CloudNode, FogROSLaunchDescription
+import fogros2
 
 
 def ami_image():
@@ -34,14 +55,22 @@ def ami_image():
 
 
 def generate_launch_description():
-    """
-    Talker example that launches the listener on AWS.
-    """
-    ld = FogROSLaunchDescription()
-    machine1 = AWS(region="us-west-1", ec2_instance_type="t2.micro", ami_image=ami_image())
+    """Talker example that launches the listener on AWS."""
+    ld = fogros2.FogROSLaunchDescription()
+    machine1 = fogros2.AWSCloudInstance(
+        region="us-west-1", ec2_instance_type="t2.micro", ami_image=ami_image()
+    )
 
-    listener_node = Node(package="fogros2_examples", executable="listener", output="screen")
-    talker_node = CloudNode(package="fogros2_examples", executable="talker", output="screen", machine=machine1)
+    listener_node = Node(
+        package="fogros2_examples", executable="listener", output="screen"
+    )
+
+    talker_node = fogros2.CloudNode(
+        package="fogros2_examples",
+        executable="talker",
+        output="screen",
+        machine=machine1,
+    )
     ld.add_action(talker_node)
     ld.add_action(listener_node)
     return ld
